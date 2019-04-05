618568eb3f66a25aec2444f5377b6828060dbe7a
BenjaminDiazGarcia <39738647+BenjaminDiazGarcia@users.noreply.github.com>
2019-04-05 11:25:26 -0500

    Add files via upload

--- Vector.cpp
+++ Vector.cpp
@@ -5,17 +5,67 @@
 #include "Vector.h"
 
 namespace UTEC {
+    int array[1];
     void vector::push_back(const int &value) {
-        // Completarlo
+        _size += 1;
+        /* *_arr2 = nullptr;
+         int array2[_size];*/
+        _arr = array;
+        
+        if (_size == 1){
+            array[0] = {value};
+        }
+        
+        else {
+            int* _arr2 = new int [_size];
+            for (int i = 0; i < _size-1; i++){
+                *(_arr2 + i) = *(_arr + i);
+            }
+            int array[_size];
+            
+            for (int i = 0; i < _size; i++){
+                *(_arr + i) = *(_arr2 + i);
+            }
+            *(_arr + _size-1) = value;
+            delete[] _arr2;
+        }
     }
 
     void vector::pop_back() {
-        // Completarlo
+        _size -= 1;
+        if (_size != 0){
+            auto *_arr2 = new int [_size];
+            for (int i = 0; i < _size-1; i++){
+                *(_arr2 + i) = *(_arr + i);
+            }
+            int array[_size];
+            
+            for (int i = 0; i < _size; i++){
+                *(_arr + i) = *(_arr2 + i);
+            }
+            delete[] _arr2;
+        }
     }
-
+    void vector::insert(int pos, const int &value){
+        _size++;
+        auto* _arr2 = new int [_size];
+        for (int i = 0; i < pos; i++){
+            *(_arr2 + i) = *(_arr + i);
+        }
+        *(_arr2 + pos) = value;
+        for (int i = pos; i < _size; i++){
+            *(_arr2 + i + 1) = *(_arr + i);
+        }
+        int array[_size];
+        for (int i = 0; i < _size; i++){
+            *(_arr + i) = *(_arr2 + i);
+        }
+        delete[] _arr2;
+    }
     vector::vector() : _arr{nullptr}, _size(0) {}
     vector::~vector() {
-        // Completarlo
+        //Creo que va esto
+        delete[] vector::_arr;
     }
 
     int vector::size() {return _size;}
